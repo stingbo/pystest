eb44cdb12796fa8d35072d9cc4888162244e4d0f
wanlianbo <wanlianbo.bj@acewill.cn>
2020-11-06 14:44:49 +0800

    修改登录脚本

--- login.py
+++ login.py
@@ -1,5 +1,12 @@
-#coding=utf-8
+# coding = utf-8
 from selenium import webdriver
 
-driver = webdriver.Chrome()
-driver.get("http://47.104.80.33:3002/#")
+browser = webdriver.Chrome()
+
+browser.get("http://47.104.80.33:3002/#/")
+
+browser.find_elements_by_class_name('form-group')[4].find_element_by_tag_name('input').send_keys('17600113608')
+browser.find_elements_by_class_name('form-group')[5].find_element_by_tag_name('input').send_keys('123456')
+
+browser.find_elements_by_class_name('form-group')[7].find_element_by_tag_name('button').click()
+#browser.quit()
