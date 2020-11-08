8526a109bd53c8394c8e69ae1063c62b660e0bc9
wanlianbo <wanlianbo.bj@acewill.cn>
2020-11-08 15:55:55 +0800

    优化写法

--- login.py
+++ login.py
@@ -1,11 +1,5 @@
 # coding = utf-8
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys #键盘按键操作
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait #等待页面加载某些元素
-
 class Login:
     def login(browser, config):
         url = config.get('WEBSITE').get('url')
--- test.py
+++ test.py
@@ -7,8 +7,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys #键盘按键操作
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait #等待页面加载某些元素
-
 from login import Login
+from baseinfo import BaseInfo
 
 # 当前脚本所在目录路径
 curpath = os.path.dirname(os.path.realpath(__file__))
@@ -22,10 +22,4 @@ browser = webdriver.Chrome()
 
 Login.login(browser, config)
 
-# 显式等待元素加载
-wait = WebDriverWait(browser, 10)
-wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'el-submenu')))
-
-browser.find_elements_by_class_name('menu-icon-4')[0].click()
-
-browser.find_element(By.XPATH, "//span[text()='企业信息']/..").click()
+BaseInfo.companyInfo(browser, config);
